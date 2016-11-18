33d1a860aee624ddad7af6c5d4f4caa466ca49d6
Shelby Clayton <shelby_clayton@hotmail.com>
2016-11-18 09:35:37 -0800

    add link from MainActivity to MenuActivity with welcome message.

--- app/src/main/java/com/epicodus/bookview/MainActivity.java
+++ app/src/main/java/com/epicodus/bookview/MainActivity.java
@@ -5,21 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSubmitButton;
+    private EditText mNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mSubmitButton = (Button) findViewById(R.id.submitButton);
+        mNameEditText = (EditText) findViewById(R.id.nameEditText);
+
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = mNameEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                intent.putExtra("name", name);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/epicodus/bookview/MenuActivity.java
+++ app/src/main/java/com/epicodus/bookview/MenuActivity.java
@@ -1,13 +1,20 @@
 package com.epicodus.bookview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
+    private TextView mWelcomeUserTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        mWelcomeUserTextView = (TextView) findViewById(R.id.welcomeUserTextView);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        mWelcomeUserTextView.setText("Welcome " + name + "!");
     }
 }
