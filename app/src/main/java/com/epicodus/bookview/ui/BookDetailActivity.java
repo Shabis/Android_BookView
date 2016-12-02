f65a988da928c0741124dcc2a1ed3e334056694a
Shelby Clayton <shelby_clayton@hotmail.com>
2016-12-02 11:12:24 -0800

    update book detail page for fragment

--- app/src/main/java/com/epicodus/bookview/ui/BookDetailActivity.java
+++ app/src/main/java/com/epicodus/bookview/ui/BookDetailActivity.java
@@ -1,15 +1,38 @@
 package com.epicodus.bookview.ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.epicodus.bookview.R;
+import com.epicodus.bookview.adapters.BookPagerAdapter;
+import com.epicodus.bookview.models.Book;
+
+import org.parceler.Parcels;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class BookDetailActivity extends AppCompatActivity {
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+    private BookPagerAdapter adapterViewPager;
+    ArrayList<Book> mBooks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+        ButterKnife.bind(this);
+
+        mBooks = Parcels.unwrap(getIntent().getParcelableExtra("books"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new BookPagerAdapter(getSupportFragmentManager(), mBooks);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
