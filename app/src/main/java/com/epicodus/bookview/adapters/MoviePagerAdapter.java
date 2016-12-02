03a013ccff1e8f8701fa8b01fe3c3a4c1bef217c
Shelby Clayton <shelby_clayton@hotmail.com>
2016-12-02 10:59:34 -0800

    add methods to adapter

--- app/src/main/java/com/epicodus/bookview/adapters/MoviePagerAdapter.java
+++ app/src/main/java/com/epicodus/bookview/adapters/MoviePagerAdapter.java
@@ -1,7 +1,37 @@
 package com.epicodus.bookview.adapters;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.epicodus.bookview.models.Book;
+import com.epicodus.bookview.ui.MovieDetailFragment;
+
+import java.util.ArrayList;
+
 /**
  * Created by Guest on 12/2/16.
  */
-public class MoviePagerAdapter {
+public class MoviePagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Book> mBooks;
+
+    public MoviePagerAdapter(FragmentManager fm, ArrayList<Book> books) {
+        super(fm);
+        mBooks = books;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return MovieDetailFragment.newInstance(mBooks.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mBooks.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mBooks.get(position).getTitle();
+    }
 }
